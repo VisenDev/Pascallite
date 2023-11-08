f1ea826c0872c4370185403e210f622c7a5d1525
Robert Burnett <dr.slate2@gmail.com>
2023-11-08 14:19:16 -0600

    finished most of nextToken

--- lexer.cpp
+++ lexer.cpp
@@ -1,49 +1,62 @@
-string nextToken() //returns the next token or end of file marker
+
+#include "stage0.h"
+#include <cctype>
+
+string Compiler::nextToken() //returns the next token or end of file marker
 {
    token = "";
-   while (token == "")
-   {
-      switch(ch)
-      {
-         case '{' : //process comment
-            while (nextChar() is not one of END_OF_FILE, '}'}
-                  { //empty body }
-                  if (ch == END_OF_FILE)
-                  processError(unexpected end of file)
-                  else
-                  nextChar()
-                  case '}' : processError('}' cannot begin token)
-                  case isspace(ch) : nextChar()
-                  case isSpecialSymbol(ch): token = ch;
-                  nextChar()
-                  case islower(ch) : token = ch;
-                  while (nextChar() is one of letter, digit, or
-                     '_' but not END_OF_FILE)
-                  {
-                  token+=ch
-                  }
-                  if (ch is END_OF_FILE)
-                  processError(unexpected end of file)
-                  case isdigit(ch) : token = ch;
-                  while (nextChar() is digit but not END_OF_FILE)
-                  {
-                     token+=ch
-                  }
-                                     if (ch is END_OF_FILE)
-                                        processError(unexpected end of file)
-                  case END_OF_FILE : token = ch
-                  default : processError(illegal symbol)
+   while (token == "") {
+      if(ch == '{') {
+         auto temp = nextChar();
+         while (temp != END_OF_FILE and temp != '}') { //empty body }
+         if (ch == END_OF_FILE) {
+            processError("unexpected end of file");
+         } else {
+            nextChar();
+         }
+      }
+   } else if(ch == '}'){
+      processError("'}' cannot begin token"); 
+   } else if (isspace(ch)) {
+      nextChar();
+   } else if (isSpecialSymbol(ch)) {
+      token = ch;
+      nextChar();
+   } else if(islower(ch)){ 
+      token = ch;
+      auto temp = nextChar();
+      while (islower(temp) or isdigit(ch) or ch == '_') {
+         token+=ch;
+      }
+      if (ch == END_OF_FILE) {
+         processError("unexpected end of file");
+      }
+   } else if (isdigit(ch)) {
+      token = ch;
+      while (isdigit(nextChar())) {
+         token+=ch;
+      }
+      if (ch == END_OF_FILE) {
+         processError("unexpected end of file");
+      }
+   } else if(ch == END_OF_FILE) {
+      token = ch;
+   } else{
+      processError("illegal symbol");
    }
-   return token
+}
+return token;
 }
 
-char nextChar() //returns the next character or end of file marker
+
+//TODO finish nextChar
+char Compiler::nextChar() //returns the next character or end of file marker
 {
- read in next character
- if end of file
- ch = END_OF_FILE //use a special character to designate end of file
- else
- ch = next character
- print to listing file (starting new line if necessary)
- return ch;
+   read in next character
+      if end of file
+         ch = END_OF_FILE //use a special character to designate end of file
+      else
+         ch = next character
+            print to listing file (starting new line if necessary)
+            return ch;
 }
