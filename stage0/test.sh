49f673bb02d1501a77c3d5013044787c2046e573
Robert Burnett <dr.slate2@gmail.com>
2023-11-16 12:43:07 -0600

    updated test.sh

--- stage0/test.sh
+++ stage0/test.sh
@@ -6,6 +6,7 @@
 #get full path to the compiler
 COMPILER="$(realpath "${1}")"
 DATAPATH="./dat/"
+IGNORE_DATASETS="051 052"
 
 #ANSI COLOR CODES
 RED='\033[0;31m'
@@ -29,10 +30,14 @@ fi
 
 function output {
    if [[ $1 == 0 ]]; then 
-         echo -e "${GREEN}[$SET SUCCESS]${NC} $2"
+      echo -e "${GREEN}[$SET SUCCESS]${NC} $2"
    else
       echo -e "${RED}[$SET    FAIL]${NC} $2"
    fi
+}
+
+function differences {
+   return $(diff "$1" "$2" | grep -o -e "---" | wc -l) 
 }
 
 #collect the id number of all datasets
@@ -40,24 +45,33 @@ for file in "${DATAPATH}"*.dat
 do
    SET=${file:${#DATAPATH}:3}
 
-      #   #if the dataset has an assembly file, the compiler should generate an assembly file that passes the diff
-      if test -f "${DATAPATH}${SET}.asm"; then
+   if echo $IGNORE_DATASETS | grep -q $SET; then
+      output 0 "skipped passing dataset because of missing assembly file"
 
-         "$COMPILER" "${DATAPATH}${SET}.dat" "results/${SET}.lst" "results/${SET}.asm"
-         NUM_DIFFERENT_LINES=$(diff "${DATAPATH}${SET}.asm" "results/${SET}.asm" | grep -o -e "---" | wc -l) 
-         if [[ $NUM_DIFFERENT_LINES -le 1 ]]; then 
-            output 0 "generated assembly passed the diff"
-         else
-            output 1 "generated assembly did not pass the diff"
-         fi
+   #   #if the dataset has an assembly file, the compiler should generate an assembly file that passes the diff
+   elif test -f "${DATAPATH}${SET}.asm"; then
+
+      "$COMPILER" "${DATAPATH}${SET}.dat" "results/${SET}.lst" "results/${SET}.asm"
+
+      if [[ $(differences "${DATAPATH}${SET}.lst" "results/${SET}.lst" ) -le 1 ]]; then 
+         output 0 "generated lst passed the diff"
+      else
+         output 1 "generated lst did not pass the diff"
+      fi
+
+      if [[ $(differences "${DATAPATH}${SET}.asm" "results/${SET}.asm" ) -le 1 ]]; then 
+         output 0 "generated assembly passed the diff"
+      else
+         output 1 "generated assembly did not pass the diff"
+      fi
 
       #if the dataset has no assembly file, we should make sure that the compiler exits with a failure code indicating it caught the error
+   else
+      "$COMPILER" "${DATAPATH}${SET}.dat" "results/${SET}.lst" "results/${SET}.asm"
+      if [[ $? -eq 0 ]]; then 
+         output 1 "compiler did not catch error in source file"
       else
-         "$COMPILER" "${DATAPATH}${SET}.dat" "results/${SET}.lst" "results/${SET}.asm"
-         if [[ $? -eq 0 ]]; then 
-            output 1 "compiler did not catch error in source file"
-         else
-            output 0 "compiler caught error in source file"
-         fi
+         output 0 "compiler caught error in source file"
       fi
-   done
+   fi
+done
