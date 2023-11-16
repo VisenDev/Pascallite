06ad36de535cac3da89e062996ee35736de9e1ca
Robert Burnett <dr.slate2@gmail.com>
2023-11-15 21:23:22 -0600

    capped external names at 15 chars

--- stage0/codegen.cpp
+++ stage0/codegen.cpp
@@ -18,6 +18,10 @@ void Compiler::insert(string externalName, storeTypes inType, modes inMode, stri
       if ((externalName != "") && (externalName.front() == ','))
          externalName.erase(0,1);
 
+      //Robert: cap name at 15 chars
+      if(name.length() > 15){
+         name = name.substr(0, 15);
+      }
 
       if (symbolTable.find(name) != symbolTable.end())
          processError("multiple name definition");
@@ -28,11 +32,11 @@ void Compiler::insert(string externalName, storeTypes inType, modes inMode, stri
          if ((name.front() >= 'A') && (name.front() <= 'Z')) {
             //symbolTable.insert(SymbolTableEntry{name,inType,inMode,inValue,inAlloc,inUnits});
             //symbolTable.find(name)->second = SymbolTableEntry{name,inType,inMode,inValue,inAlloc,inUnits};
-			symbolTable.insert(pair<string, SymbolTableEntry>(name, SymbolTableEntry(name,inType,inMode,inValue,inAlloc,inUnits)));
+            symbolTable.insert(pair<string, SymbolTableEntry>(name, SymbolTableEntry(name,inType,inMode,inValue,inAlloc,inUnits)));
          } else {
             //symbolTable.find(name)->second = SymbolTableEntry{genInternalName(inType),inType,inMode,inValue,inAlloc,inUnits};
             //symbolTable[name]= SymbolTableEntry(genInternalName(inType),inType,inMode,inValue,inAlloc,inUnits);
-			symbolTable.insert(pair<string, SymbolTableEntry>(name, SymbolTableEntry(genInternalName(inType),inType,inMode,inValue,inAlloc,inUnits)));
+            symbolTable.insert(pair<string, SymbolTableEntry>(name, SymbolTableEntry(genInternalName(inType),inType,inMode,inValue,inAlloc,inUnits)));
          }  //Matt:  Not sure if this is gonna work, I don't think name has been inserted to the symbolTable yet and symbolTable.find(name) will always return null ptr
       }
    }
@@ -107,64 +111,64 @@ string Compiler::whichValue(string name) //tells which value a name has
 }
 
 void Compiler::code(string op, string operand1, string operand2) {
-	(void) operand2; //suppress warning
-	if (op == "program") {
-		emitPrologue(operand1);
+   (void) operand2; //suppress warning
+   if (op == "program") {
+      emitPrologue(operand1);
    } else if (op == "end") {
-		emitEpilogue();
+      emitEpilogue();
    } else {
-		processError("compiler error since function code should not be called with illegal arguments");
+      processError("compiler error since function code should not be called with illegal arguments");
    }
 }
 
 void Compiler::emit(string label, string instruction, string operands, string comment) {	
-	if (operands == "true")
-		operands = "-1";
-	if (operands == "false")
-		operands = "0";
-	objectFile << left;
-	objectFile << setw(8) << label << setw(8) << instruction << setw(24) << operands <<  comment << endl;
+   if (operands == "true")
+      operands = "-1";
+   if (operands == "false")
+      operands = "0";
+   objectFile << left;
+   objectFile << setw(8) << label << setw(8) << instruction << setw(24) << operands <<  comment << endl;
 }
 
 void Compiler::emitPrologue(string progName, string operand2) {
    (void) operand2; //suppress warning
-	time_t now = time (NULL);
-	objectFile << left << setw(36) << "; Robert Burnett, Matthew Barton" << setw(0) << ctime(&now);
-	
-	objectFile << "%INCLUDE \"Along32.inc\"" << endl;
-	objectFile << "%INCLUDE \"Macros_Along.inc\"" << endl << endl;
-	
-	emit("SECTION", ".text");
-	emit("global", "_start", "", "; program " + progName);
-	objectFile << endl;
-	emit("_start:");
+   time_t now = time (NULL);
+   objectFile << left << setw(36) << "; Robert Burnett, Matthew Barton" << setw(0) << ctime(&now);
+
+   objectFile << "%INCLUDE \"Along32.inc\"" << endl;
+   objectFile << "%INCLUDE \"Macros_Along.inc\"" << endl << endl;
+
+   emit("SECTION", ".text");
+   emit("global", "_start", "", "; program " + progName);
+   objectFile << endl;
+   emit("_start:");
 }
 
 void Compiler::emitEpilogue(string operand1, string operand2) {
    (void) operand1; //suppress warning
    (void) operand2; //suppress warning
-	emit("","Exit", "{0}");
-	emitStorage();
+   emit("","Exit", "{0}");
+   emitStorage();
 }
 
 void Compiler::emitStorage()
 {
-	objectFile << endl;
-	emit("SECTION", ".data");
-	
-	for (auto itr = symbolTable.begin(); itr != symbolTable.end(); ++itr)
-		if ((itr->second.getAlloc() == YES) && (itr->second.getMode() == CONSTANT))
-		{
-			emit(itr->second.getInternalName(), "dd", itr->second.getValue(), "; "+itr->first);
-		}
-	
-	objectFile << endl;
-	emit("SECTION", ".bss");
-	
-	for (auto itr = symbolTable.begin(); itr != symbolTable.end(); ++itr)
-		if ((itr->second.getAlloc() == YES) && (itr->second.getMode() == VARIABLE))
-		{
-			emit(itr->second.getInternalName(), "resd", "1" , "; "+itr->first); // I hard coded in 1 for units, but this needs to be changed later I think
-		}
-	
+   objectFile << endl;
+   emit("SECTION", ".data");
+
+   for (auto itr = symbolTable.begin(); itr != symbolTable.end(); ++itr)
+      if ((itr->second.getAlloc() == YES) && (itr->second.getMode() == CONSTANT))
+      {
+         emit(itr->second.getInternalName(), "dd", itr->second.getValue(), "; "+itr->first);
+      }
+
+   objectFile << endl;
+   emit("SECTION", ".bss");
+
+   for (auto itr = symbolTable.begin(); itr != symbolTable.end(); ++itr)
+      if ((itr->second.getAlloc() == YES) && (itr->second.getMode() == VARIABLE))
+      {
+         emit(itr->second.getInternalName(), "resd", "1" , "; "+itr->first); // I hard coded in 1 for units, but this needs to be changed later I think
+      }
+
 }
