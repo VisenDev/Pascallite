60fab9f6eefb6e1da09b26ec31e71c0369ba302d
matt <mbarton7@angelo.edu>
2023-11-15 19:26:37 -0600

    I think I'm adding the dat files now (?)

---
+++ stage0/dat/003.asm
@@ -0,0 +1,8 @@
+; YOUR NAME(S)       Mon Nov  7 15:02:52 2022
+%INCLUDE "Along32.inc"
+%INCLUDE "Macros_Along.inc"
+
+SECTION .text                           
+global  _start                          ; program stage0no003
+
+_start:                                 
