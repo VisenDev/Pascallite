52c66bc92fb5e6f6b98c25452a3ea2606da15cc2
matt <mbarton7@angelo.edu>
2023-12-09 12:19:01 -0600

    wrote stack operations

--- stage1/actions.cpp
+++ stage1/actions.cpp
@@ -1,6 +1,26 @@
 #include "stage1.h"
 
-void   Compiler::pushOperator(string op){}
-string Compiler::popOperator(){}
-void   Compiler::pushOperand(string operand){}
-string Compiler::popOperand(){}
+void   Compiler::pushOperator(string op)
+{
+	operatorStk.push(op);
+}
+string Compiler::popOperator()
+{
+	if (operatorStk.empty())
+		processError("compiler error; operator stack underflow");
+	string temp = operatorStk.top();
+	operatorStk.pop();
+	return temp;
+}
+void   Compiler::pushOperand(string op)
+{
+	operandStk.push(op);
+}
+string Compiler::popOperand()
+{
+	if (operandStk.empty())
+		processError("compiler error; operand stack underflow");
+	string temp = operandStk.top();
+	operandStk.pop();
+	return temp;
+}
