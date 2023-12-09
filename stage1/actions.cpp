d393f5cc530525f2f46c8dde01fdad0ab95ce740
Robert Burnett <dr.slate2@gmail.com>
2023-12-09 13:05:34 -0600

    more progress on parsers

--- stage1/actions.cpp
+++ stage1/actions.cpp
@@ -1,26 +1,27 @@
 #include "stage1.h"
 
-void   Compiler::pushOperator(string op)
-{
-	operatorStk.push(op);
+void Compiler::pushOperator(string op) {
+   operatorStk.push(op);
 }
-string Compiler::popOperator()
-{
-	if (operatorStk.empty())
-		processError("compiler error; operator stack underflow");
-	string temp = operatorStk.top();
-	operatorStk.pop();
-	return temp;
+
+string Compiler::popOperator() {
+   if (operatorStk.empty()) {
+      processError("compiler error; operator stack underflow");
+   }
+   string temp = operatorStk.top();
+   operatorStk.pop();
+   return temp;
 }
-void   Compiler::pushOperand(string op)
-{
-	operandStk.push(op);
+
+void Compiler::pushOperand(string op) {
+   operandStk.push(op);
 }
-string Compiler::popOperand()
-{
-	if (operandStk.empty())
-		processError("compiler error; operand stack underflow");
-	string temp = operandStk.top();
-	operandStk.pop();
-	return temp;
+
+string Compiler::popOperand() {
+   if (operandStk.empty()){
+      processError("compiler error; operand stack underflow");
+   }
+   string temp = operandStk.top();
+   operandStk.pop();
+   return temp;
 }
